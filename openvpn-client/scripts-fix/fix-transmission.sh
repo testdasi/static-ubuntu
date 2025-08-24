9b96a424dbfc2d653838229cb421436849ae946f
testdasi <51292604+testdasi@users.noreply.github.com>
2025-08-24 12:43:06 +0100

    cp --update=none

--- openvpn-client/scripts-fix/fix-transmission.sh
+++ openvpn-client/scripts-fix/fix-transmission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /config/transmission \
-    && cp -n /static-ubuntu/openvpn-client/etc/transmission.json /config/transmission/settings.json \
+    && cp --update=none /static-ubuntu/openvpn-client/etc/transmission.json /config/transmission/settings.json \
     && mkdir -p /data/transmission/watch \
     && mkdir -p /data/transmission/incomplete \
     && mkdir -p /data/transmission/complete
