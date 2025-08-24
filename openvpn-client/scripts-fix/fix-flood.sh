2e447597bf5e04bcb4e855e9098c127ddf4d5899
testdasi <51292604+testdasi@users.noreply.github.com>
2025-08-24 12:46:58 +0100

    cp --update=none

--- openvpn-client/scripts-fix/fix-flood.sh
+++ openvpn-client/scripts-fix/fix-flood.sh
@@ -6,6 +6,6 @@ sed -i "s|_FLOOD_PORT_|$FLOOD_PORT|g" '/nftables.rules'
 sed -i "s|:3000|:$FLOOD_PORT|g" '/app/launcher/index.html'
 
 mkdir -p /config/flood/db \
-    && cp -n /static-ubuntu/openvpn-client/etc/flood.db /config/flood/db/users.db
+    && cp --update=none /static-ubuntu/openvpn-client/etc/flood.db /config/flood/db/users.db
 echo 'default flood login is admin:flood unless changed in settings'
 echo '[info] flood fixed.'
