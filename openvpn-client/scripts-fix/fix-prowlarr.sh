7b661523bfd6ea5a0165620f4a16e5b8ebc87e7f
testdasi <51292604+testdasi@users.noreply.github.com>
2025-08-24 12:45:21 +0100

    cp --update=none

--- openvpn-client/scripts-fix/fix-prowlarr.sh
+++ openvpn-client/scripts-fix/fix-prowlarr.sh
@@ -5,6 +5,6 @@ sed -i "s|_PROWLARR_PORT_|$PROWLARR_PORT|g" '/nftables.rules'
 sed -i "s|:9696|:$PROWLARR_PORT|g" '/app/launcher/index.html'
 
 mkdir -p /config/prowlarr \
-    && cp -n /static-ubuntu/openvpn-client/etc/prowlarr.xml /config/prowlarr/config.xml
+    && cp --update=none /static-ubuntu/openvpn-client/etc/prowlarr.xml /config/prowlarr/config.xml
 sed -i "s|9696|$PROWLARR_PORT|g" '/config/prowlarr/config.xml'
 echo '[info] prowlarr fixed.'
