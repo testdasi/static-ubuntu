2903290ad8c1ea9d924fb218424624c9a93ef985
testdasi <51292604+testdasi@users.noreply.github.com>
2025-08-24 12:47:45 +0100

    cp --update=none

--- grafana-unraid-stack/initialise.sh
+++ grafana-unraid-stack/initialise.sh
@@ -6,7 +6,7 @@ mkdir -p /config/influxdb \
     && mkdir -p /data/influxdb/wal \
     && rm -f /config/influxdb/influxd.pid \
     && rm -f /config/influxdb/influxd.log \
-    && cp -n /static-ubuntu/grafana-unraid-stack/influxdb/influxdb.conf /config/influxdb/
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/influxdb/influxdb.conf /config/influxdb/
 sed -i "s|:8086|:$INFLUXDB_HTTP_PORT|g" '/config/influxdb/influxdb.conf'
 sed -i "s|:8088|:$INFLUXDB_RPC_PORT|g" '/config/influxdb/influxdb.conf'
 echo '[info] influxdb fixed.'
@@ -14,7 +14,7 @@ echo '[info] influxdb fixed.'
 mkdir -p /config/loki \
     && mkdir -p /data/loki/chunks \
     && mkdir -p /data/loki/index \
-    && cp -n /static-ubuntu/grafana-unraid-stack/loki/loki-local-config.yaml /config/loki/
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/loki/loki-local-config.yaml /config/loki/
 sed -i "s|: 3100|: $LOKI_PORT|g" '/config/loki/loki-local-config.yaml'
 echo '[info] loki fixed.'
 
@@ -23,18 +23,18 @@ mkdir -p /config/telegraf/telegraf.d \
 if [[ $USE_HDDTEMP =~ "yes" ]]
 then
     mkdir -p /config/hddtemp \
-        && cp -n /static-ubuntu/grafana-unraid-stack/hddtemp/hddtemp.db /config/hddtemp/
+        && cp --update=none /static-ubuntu/grafana-unraid-stack/hddtemp/hddtemp.db /config/hddtemp/
     echo '[info] hddtemp fixed.'
-    cp -n /static-ubuntu/grafana-unraid-stack/telegraf/telegraf-hddtemp.conf /config/telegraf/telegraf.conf
+    cp --update=none /static-ubuntu/grafana-unraid-stack/telegraf/telegraf-hddtemp.conf /config/telegraf/telegraf.conf
 else
-    cp -n /static-ubuntu/grafana-unraid-stack/telegraf/telegraf-smart.conf /config/telegraf/telegraf.conf
+    cp --update=none /static-ubuntu/grafana-unraid-stack/telegraf/telegraf-smart.conf /config/telegraf/telegraf.conf
 fi
 sed -i "s|127\.0\.0\.1:8086|127\.0\.0\.1:$INFLUXDB_HTTP_PORT|g" '/config/telegraf/telegraf.conf'
 echo '[info] telegraf fixed.'
 
 mkdir -p /config/promtail \
     && mkdir -p /data/promtail \
-    && cp -n /static-ubuntu/grafana-unraid-stack/promtail/promtail.yml /config/promtail/
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/promtail/promtail.yml /config/promtail/
 sed -i "s|127\.0\.0\.1:3100|127\.0\.0\.1:$LOKI_PORT|g" '/config/promtail/promtail.yml'
 sed -i "s|http_listen_port: 9080|http_listen_port: $PROMTAIL_PORT|g" '/config/promtail/promtail.yml'
 echo '[info] promtail fixed.'
@@ -47,10 +47,10 @@ mkdir -p /config/grafana/data/plugins \
     && mkdir -p /config/grafana/provisioning/notifiers \
     && mkdir -p /config/grafana/provisioning/plugins \
     && rm -f /config/grafana/grafana-server.pid \
-    && cp -n /static-ubuntu/grafana-unraid-stack/grafana/provisioning/datasources/* /config/grafana/provisioning/datasources/ \
-    && cp -n /static-ubuntu/grafana-unraid-stack/grafana/provisioning/dashboards/* /config/grafana/provisioning/dashboards/ \
-    && cp -n /static-ubuntu/grafana-unraid-stack/grafana/data/dashboards/* /config/grafana/data/dashboards/ \
-    && cp -n /static-ubuntu/grafana-unraid-stack/grafana/grafana.ini /config/grafana/
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/grafana/provisioning/datasources/* /config/grafana/provisioning/datasources/ \
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/grafana/provisioning/dashboards/* /config/grafana/provisioning/dashboards/ \
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/grafana/data/dashboards/* /config/grafana/data/dashboards/ \
+    && cp --update=none /static-ubuntu/grafana-unraid-stack/grafana/grafana.ini /config/grafana/
 sed -i "s| 3000| $GRAFANA_PORT|g" '/config/grafana/grafana.ini'
 sed -i "s|:8086|:$INFLUXDB_HTTP_PORT|g" '/config/grafana/provisioning/datasources/InfluxDB-Telegraf.yml'
 sed -i "s|:3100|:$LOKI_PORT|g" '/config/grafana/provisioning/datasources/Loki.yml'
